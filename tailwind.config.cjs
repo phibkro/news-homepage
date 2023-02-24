2e4f71eeb22fe3ae3baef25c6388f8023eaaa3b7
Skuiggly <piplup107@gmail.com>
2023-02-24 14:51:51 +0100

    fix: colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,23 +5,24 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    screens: {
-      mobile: "375px",
-      desktop: "1440px",
-    },
-    colors: {
-      primary_orange: "E9AB53",
-      primary_red: "F15E50",
+    extend: {
+      colors: {
+        primary_orange: "hsl(35, 77%, 62%)",
+        primary_red: "hsl(5, 85%, 63%)",
 
-      off_white: "FFFDFA",
-      grayish_blue: "C5C6CE",
-      dark_grayish_blue: "5D5F79",
-      very_dark_blue: "000019",
+        off_white: "hsl(36, 100%, 99%)",
+        grayish_blue: "hsl(233, 8%, 79%)",
+        dark_grayish_blue: "hsl(236, 13%, 42%)",
+        very_dark_blue: "hsl(240, 100%, 5%)",
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      fontFamily: {
+        sans: ['Inter, sans-serif'],
+      },
     },
-    fontFamily: {
-      sans: ['Inter, sans-serif'],
-    },
-    extend: {},
   },
   plugins: [],
 }
