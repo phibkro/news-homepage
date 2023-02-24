e377c0522b7f5d56ea34f48f0939dcdc799985e4
Skuiggly <piplup107@gmail.com>
2023-02-24 04:20:40 +0100

    init: vite + react + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-slate-700 h-screen flex justify-center items-center">
+      <h1>Vite + React + Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
