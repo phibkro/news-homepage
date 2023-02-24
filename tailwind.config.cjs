087aa58b2b5bd97db69aea18365e87f585520873
Skuiggly <piplup107@gmail.com>
2023-02-24 04:44:59 +0100

    feat: configure style guide

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
+    colors: {
+      primary_orange: "E9AB53",
+      primary_red: "F15E50",
+
+      off_white: "FFFDFA",
+      grayish_blue: "C5C6CE",
+      dark_grayish_blue: "5D5F79",
+      very_dark_blue: "000019",
+    },
+    fontFamily: {
+      sans: ['Inter, sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
