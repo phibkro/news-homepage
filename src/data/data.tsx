6812d7e43a0658084bd15c1babb73ff1c7ec5ea3
Skuiggly <piplup107@gmail.com>
2023-02-25 01:22:02 +0100

    refactor data into own folder

---
+++ src/data/data.tsx
@@ -0,0 +1,42 @@
+export const featuredData = {
+  title: "The Bright Future of Web 3.0?",
+  content: "We dive into the next evolution of the web that claims to put the power of platforms back into the hands of the people. But is it really fulfilling its promise?",
+  src: "/images/image-web-3-mobile.jpg",
+  alt: "Colorful blocks"
+}
+export const newData = [{
+    title: "Hydrogen VS Electric Cars",
+    content: "Will hydrogen-fueled cars ever catch up to EVs?"
+  },
+  {
+    title: "The Downsides of AI Artistry",
+    content: "What are the possible adverse effects of on-demand AI image generation?",
+  },
+  {
+    title: "Is VC Funding Drying Up?",
+    content: "Private funding by VC firms is down 50% YOY. We take a look at what that means",
+  },
+]
+export const numberedData = [
+  {
+    title: "Reviving Retro PCs",
+    content: "What happens when old PCs are given modern upgrades?",
+    index: "01",
+    src: "/images/image-retro-pcs.jpg",
+    alt: "A small retro pc surrounded by old technology",
+  },
+  {
+    title: "Top 10 Laptops of 2022",
+    content: "Our best picks for various needs and budgets",
+    index: "02",
+    src: "/images/image-top-laptops.jpg",
+    alt: "Close-up of keycaps on a laptop"
+  },
+  {
+    title: "The Growth of Gaming",
+    content: "How the pandemic has sparked fresh opportunities",
+    index: "03",
+    src: "/images/image-gaming-growth.jpg",
+    alt: "A hand catching a Playstation 5 controller",
+  }
+]
